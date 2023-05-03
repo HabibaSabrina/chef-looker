55bc0bc2d1324821508053dc903789155c19c825
HabibaSabrina <habibasabrina92@gmail.com>
2023-05-03 12:45:36 +0600

    banner is added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <div className='flex gap-16 items-center'>
                 <div className='flex items-center'>
                     <img className='w-36' src="logo.png" alt="" />
-                    <h1 className='text-3xl font-bold -m-8'>Chef Cooker</h1>
+                    <h1 className='text-3xl font-bold -m-8'>Chef Looker</h1>
                 </div>
                 <div>
                     <Link to='/'><button className='mx-5 bg-orange-300 w-24 py-3 rounded text-xl font-semibold text-white'>Home</button></Link>
