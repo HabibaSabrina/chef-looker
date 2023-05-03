8801c3a66525069eecb55151d6287129cf3dfa91
HabibaSabrina <habibasabrina92@gmail.com>
2023-05-03 22:55:28 +0600

    firebase is initialized

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -8,16 +8,16 @@ const Register = () => {
             <h1 className='text-center text-4xl font-bold text-orange-700'>Register</h1>
             <div className='text-center mt-10'>
             <p className='font-semibold '>Name</p>
-                <input className='mb-5 border-b-2 focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="text" name="" id="" />
+                <input className='mb-5 border-b-2 focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="text" name="name" />
                 <br />
                 <p className='font-semibold mt-5'>Email</p>
-                <input className='border-b-2 focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="email" name="" id="" />
+                <input className='border-b-2 focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="email" name="email" />
                 <br />
                 <p className='font-semibold mt-10'>Password</p>
-                <input className='border-b-2 focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="password" name="" id="" />
+                <input className='border-b-2 focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="password" name="password" />
                 <br />
                 <p className='font-semibold mt-10'>Photo URL</p>
-                <input className='border-b-2 focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="password" name="" id="" />
+                <input className='border-b-2 focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="password" name="photo" />
                 <br />
                 <button className='bg-orange-400 w-64 rounded-full p-3 text-xl text-white font-semibold hover:bg-orange-500 mt-10'>Register</button>
                 <p className='my-5'>Already Have an Account? <Link to="/login"><span className='text-orange-700 font-semibold'>Login</span></Link></p>
