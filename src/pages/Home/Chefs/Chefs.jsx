8c190bb58cc76430f16cf7a9cd6b522d0a1c04de
HabibaSabrina <habibasabrina92@gmail.com>
2023-05-03 13:08:06 +0600

    chef book title is added

--- src/pages/Home/Chefs/Chefs.jsx
+++ src/pages/Home/Chefs/Chefs.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Chefs = () => {
     return (
-        <div>
-            
+        <div className='mx-32'>
+            <div className='flex items-center justify-center mt-20 mb-5'>
+            <h1 className='text-4xl font-bold'>Bangladeshi Chef Book</h1>
+            <img className='w-24 h-20 -ml-3' src="bdchef.png" alt="" />
+            </div>
+            <p className='text-center'>In this section, you will get to know some amazing chefs of our country whose are making their remarks allover the world. Surely they are making us proud. You also can visit their recipes from our website.</p>
         </div>
     );
 };
