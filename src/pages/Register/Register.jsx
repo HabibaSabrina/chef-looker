a041e90b7ce792864456a28d98cc0f399d738403
HabibaSabrina <habibasabrina92@gmail.com>
2023-05-04 02:18:22 +0600

    user login and logout is working

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,24 +1,54 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
+import { updateProfile } from 'firebase/auth';
 
 const Register = () => {
     const { createUser } = useContext(AuthContext)
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
     const handleRegister = event => {
         event.preventDefault();
+        setSuccess('')
+        setError('')
         const form = event.target;
         const name = form.name.value;
         const photo = form.photo.value;
         const email = form.email.value;
         const password = form.password.value;
+        
+        if (password.length < 6) {
+            setError('Please add at least 6 characters in your password')
+            return;
+        }
+        else if(email =='' || password==''){
+            setError('Please fill up both email and password field')
+        }
         createUser(email, password)
         .then(result =>{
             const createdUser = result.user;
-            console.log(createUser)
+            console.log(createdUser)
+            setError('')
+            event.target.reset()
+            setSuccess('user has created successfully')
+            updateUserData(result.user, photo)
         })
         .catch(error =>{
-            console.log(error)
+            setError(error.message)
+            setSuccess('')
         })
+        const updateUserData = (user, photo) =>{
+            updateProfile(user, {
+                photoURL: photo
+              })
+              .then(() => {
+                console.log("user name updated")
+              })
+              .catch((error) => {
+                setError(error.message)
+              });
+              
+        }
     }
 
     return (
@@ -31,10 +61,10 @@ const Register = () => {
                     <input className='text-center mb-5 border-b-2 focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="text" name="name" />
                     <br />
                     <p className='font-semibold mt-5'>Email</p>
-                    <input className='text-center border-b-2 focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="email" name="email" required />
+                    <input className='text-center border-b-2 focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="email" name="email" />
                     <br />
                     <p className='font-semibold mt-10'>Password</p>
-                    <input className='border-b-2 text-center focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="password" name="password" required />
+                    <input className='border-b-2 text-center focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="password" name="password" />
                     <br />
                     <p className='font-semibold mt-10'>Photo URL</p>
                     <input className='text-center border-b-2 focus:outline-0 focus:text-center border-orange-300 mt-5 w-80' type="text" name="photo" />
@@ -43,6 +73,8 @@ const Register = () => {
                     <p className='my-5'>Already Have an Account? <Link to="/login"><span className='text-orange-700 font-semibold'>Login</span></Link></p>
                 </div>
             </form>
+            <p className='text-center text-red-500 font-semibold'>{error}</p>
+            <p className='text-center text-green-500 font-semibold'>{success}</p>
         </div>
     );
 };
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { ArrowRightOnRectangleIcon } from '@heroicons/react/24/solid'
+import { ArrowRightOnRectangleIcon, ArrowLeftOnRectangleIcon } from '@heroicons/react/24/solid'
+import { AuthContext } from '../../../providers/AuthProvider';
+import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext)
+    const handleLogOut = () =>{
+        logOut()
+        .then()
+        .catch(error =>{
+            console.log(error)
+        })
+    }
     return (
         <div className='flex justify-between items-center mx-24 mt-10'>
             <div className='flex gap-16 items-center'>
@@ -15,7 +25,15 @@ const Header = () => {
                     <Link to='/blog'><button className='bg-orange-400 w-24 py-3 rounded text-xl font-semibold text-white'>Blog</button></Link>
                 </div>
             </div>
-            <Link to="/login"><button className='bg-orange-400 w-32 p-3 rounded text-xl font-semibold text-white flex items-center gap-2 mr-8'><ArrowRightOnRectangleIcon className="h-6 w-7 text-white" /> Login</button></Link>
+            <div className='flex gap-5'>
+                {
+                    user && <img className='w-12 rounded-full' src={user.photoURL} alt="" />
+
+                }
+            {
+                user ? <button onClick={handleLogOut} className='bg-orange-400 w-36 p-3 rounded text-xl font-semibold text-white flex items-center gap-2 mr-8'><ArrowLeftOnRectangleIcon className="h-6 w-7 text-white" /> Log Out</button> : <Link to="/login"><button className='bg-orange-400 w-36 p-3 rounded text-xl font-semibold text-white flex items-center gap-2 mr-8'><ArrowRightOnRectangleIcon className="h-6 w-7 text-white" /> Login</button></Link>
+            }
+            </div>
         </div>
     );
 };
