306c666ebe0bb18512a91da9b7dc5c74330d593d
HabibaSabrina <habibasabrina92@gmail.com>
2023-05-03 09:26:45 +0600

    routes is established

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,26 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home/Home/Home";
+import Blog from "../pages/Home/Blog/Blog";
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path:'/',
+            element:<Home></Home>
+        },
+        {
+            path:"/blog",
+            element:<Blog></Blog>
+        }
+      ]
+    },
+  ]);
+
+export default router;
