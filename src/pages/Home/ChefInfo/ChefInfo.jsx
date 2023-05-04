f8cd543e4cb54e1c45c25cd2b5cca81ad9b15721
HabibaSabrina <habibasabrina92@gmail.com>
2023-05-04 20:52:42 +0600

    lazy load is added

--- src/pages/Home/ChefInfo/ChefInfo.jsx
+++ src/pages/Home/ChefInfo/ChefInfo.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {  HandThumbUpIcon } from '@heroicons/react/24/solid';
+import { HandThumbUpIcon } from '@heroicons/react/24/solid';
+import LazyLoad from 'react-lazy-load';
 
 const ChefInfo = ({ chef }) => {
-    const { id, chefPicture, chefName, experience, likes} = chef
+    const { id, chefPicture, chefName, experience, likes } = chef
     return (
         <div className='w-80 p-5 max-sm:my-5 border-2 rounded-xl border-orange-300 hover:bg-orange-50'>
-            <img className='rounded-full w-36 mx-auto my-5' src={chefPicture} alt="" />
+            <LazyLoad height={150}>
+                <img className='rounded-full w-36 mx-auto my-5' src={chefPicture} alt="" />
+            </LazyLoad>
             <img className='w-48 mx-auto' src="/wave.png" alt="" />
             <div className='text-center my-5'>
                 <h1 className='text-3xl font-bold'>{chefName}</h1>
